73f3e53777a29e0437704ce52e9f8ed6adcb7f7f
infinitepure <matt.j.cheah@gmail.com>
2017-11-19 23:34:40 +1100

    Create viewsets and map to url api endpoints

---
+++ tracker/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from tracker import views
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'countdowns', views.CountdownViewSet)
+router.register(r'tomatoes', views.TomatoViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api_auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
+
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from tracker.models import Countdown, Tomato
+from tracker.serializers import CountdownSerializer, TomatoSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import permissions, viewsets
 
-# Create your views here.
+class CountdownViewSet(viewsets.ModelViewSet):
+    """
+    Allows for `list`, `create`, `retrieve`, `update`,
+    and `destroy` actions for Countdowns.
+    """
+    queryset = Countdown.objects.all().order_by('due')
+    serializer_class = CountdownSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TomatoViewSet(viewsets.ModelViewSet):
+    """
+    Allows for `list`, `create`, `retrieve`, `update`,
+    and `destroy` actions for Tomatoes.
+    """
+    queryset = Tomato.objects.all()
+    serializer_class = TomatoSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Allows for `list` and `detail` actions for users.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer

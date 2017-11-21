cadd4ff58a39026fe2150fc3c2a199b859e43456
infinitepure <matt.j.cheah@gmail.com>
2017-11-21 23:24:10 +1100

    Modify the CountdownViewSet to use the full queryset but override get_queryset method

--- tracker/views.py
+++ tracker/views.py
@@ -3,7 +3,7 @@ from tracker.serializers import CountdownSerializer, TomatoSerializer, UserSeria
 from tracker.permissions import IsUser, IsUserAccount
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, mixins
 from rest_framework.response import Response
 
 class CountdownViewSet(viewsets.ModelViewSet):
@@ -11,10 +11,13 @@ class CountdownViewSet(viewsets.ModelViewSet):
     Allows for `list`, `create`, `retrieve`, `update`,
     and `destroy` actions for Countdowns.
     """
-    queryset = Countdown.objects.all().order_by('due')
+    queryset = Countdown.objects.all()
     serializer_class = CountdownSerializer
     permission_classes = (permissions.IsAuthenticated,
                           IsUser)
+
+    def get_queryset(self):
+        return self.queryset.order_by('due')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -34,15 +37,11 @@ class TomatoViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class UserViewSet(viewsets.GenericViewSet):
+class UserViewSet(mixins.RetrieveModelMixin,
+                  viewsets.GenericViewSet):
     """
     Allows for the `retrieve` action for users.
     """
     queryset = User.objects.all()
     permission_classes = (permissions.IsAuthenticated,
                           IsUserAccount)
-
-    def retrieve(self, request, pk=None):
-        user = self.get_object()
-        serializer = UserSerializer(user, context={'request': request})
-        return Response(serializer.data)

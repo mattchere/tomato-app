a7a385aa63691e1b609ca931d74a05ac645ca162
infinitepure <matt.j.cheah@gmail.com>
2017-11-24 00:33:10 +1100

    Add modal form that creates new countdowns

--- tracker/static/js/countdown.js
+++ tracker/static/js/countdown.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  var countdowns;
+  
   $.get(
-    '/api/v1/countdowns',
+    '/api/v1/countdowns/',
     function(data) {
       data.forEach(function(countdown) {
         var splitDate = countdown.due.split('-');
@@ -20,7 +20,25 @@ $(document).ready(function() {
       })
     }
   )
+
+  $('#add-countdown').submit(function(e) {
+    
+    e.preventDefault();
+
+    setCSRFToken();
+
+    $.ajax({
+      type: $(this).attr('method'),
+      url: $(this).attr('action'),
+      data: createCountdown($(this).serializeArray()),
+      success: function(data) {
+        location.reload();
+      }
+    });
+
+  })
   
+
   function daysUntil(date) {
     var today = new Date(
       new Date().getFullYear(),
@@ -29,5 +47,48 @@ $(document).ready(function() {
     )
     return Math.floor((date-today) / (1000*60*60*24));
   }
+
+  
+  function createCountdown(array) {
+    var obj = {};
+    array.forEach(function(item) {
+      obj[item.name] = item.value;
+    });
+    return obj;
+  }
+
+
+  function setCSRFToken() {
+    // using jQuery
+    function getCookie(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie !== '') {
+          var cookies = document.cookie.split(';');
+          for (var i = 0; i < cookies.length; i++) {
+              var cookie = jQuery.trim(cookies[i]);
+              // Does this cookie string begin with the name we want?
+              if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+      // these HTTP methods do not require CSRF protection
+      return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+  }
+
   
 });

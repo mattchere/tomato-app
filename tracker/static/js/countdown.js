0bf54294ea7a9fd3e23f5132b083bf3de22af56b
infinitepure <matt.j.cheah@gmail.com>
2017-11-26 01:11:21 +1100

    Removed unnecessary document ready callback for countdown

--- tracker/static/js/countdown.js
+++ tracker/static/js/countdown.js
@@ -1,94 +1,89 @@
-$(document).ready(function() {
+$.get(
+  '/api/v1/countdowns/',
+  function(data) {
+    data.forEach(function(countdown) {
+      var splitDate = countdown.due.split('-');
+      var dateDue = new Date(
+        splitDate[0], 
+        splitDate[1]-1, 
+        splitDate[2]
+      );
+      $('.countdown-list').append(
+        '<li class="countdown-item">' +
+        countdown.title +
+        ' ' +
+        daysUntil(dateDue) +
+        '</li>'
+      )
+    })
+  }
+)
+
+$('#add-countdown').submit(function(e) {
   
-  $.get(
-    '/api/v1/countdowns/',
-    function(data) {
-      data.forEach(function(countdown) {
-        var splitDate = countdown.due.split('-');
-        var dateDue = new Date(
-          splitDate[0], 
-          splitDate[1]-1, 
-          splitDate[2]
-        );
-        $('.countdown-list').append(
-          '<li class="countdown-item">' +
-          countdown.title +
-          ' ' +
-          daysUntil(dateDue) +
-          '</li>'
-        )
-      })
+  e.preventDefault();
+
+  setCSRFToken();
+
+  $.ajax({
+    type: $(this).attr('method'),
+    url: $(this).attr('action'),
+    data: createCountdown($(this).serializeArray()),
+    success: function(data) {
+      location.reload();
     }
+  });
+
+})
+
+
+function daysUntil(date) {
+  var today = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    new Date().getDate()
   )
-
-  $('#add-countdown').submit(function(e) {
-    
-    e.preventDefault();
-
-    setCSRFToken();
-
-    $.ajax({
-      type: $(this).attr('method'),
-      url: $(this).attr('action'),
-      data: createCountdown($(this).serializeArray()),
-      success: function(data) {
-        location.reload();
-      }
-    });
-
-  })
-  
-
-  function daysUntil(date) {
-    var today = new Date(
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate()
-    )
-    return Math.floor((date-today) / (1000*60*60*24));
-  }
-
-  
-  function createCountdown(array) {
-    var obj = {};
-    array.forEach(function(item) {
-      obj[item.name] = item.value;
-    });
-    return obj;
-  }
+  return Math.floor((date-today) / (1000*60*60*24));
+}
 
 
-  function setCSRFToken() {
-    // using jQuery
-    function getCookie(name) {
-      var cookieValue = null;
-      if (document.cookie && document.cookie !== '') {
-          var cookies = document.cookie.split(';');
-          for (var i = 0; i < cookies.length; i++) {
-              var cookie = jQuery.trim(cookies[i]);
-              // Does this cookie string begin with the name we want?
-              if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                  break;
-              }
-          }
-      }
-      return cookieValue;
-    }
-    var csrftoken = getCookie('csrftoken');
+function createCountdown(array) {
+  var obj = {};
+  array.forEach(function(item) {
+    obj[item.name] = item.value;
+  });
+  return obj;
+}
 
-    function csrfSafeMethod(method) {
-      // these HTTP methods do not require CSRF protection
-      return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+
+function setCSRFToken() {
+  // using jQuery
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
             }
         }
-    });
+    }
+    return cookieValue;
   }
+  var csrftoken = getCookie('csrftoken');
 
-  
-});
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+  $.ajaxSetup({
+      beforeSend: function(xhr, settings) {
+          if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+              xhr.setRequestHeader("X-CSRFToken", csrftoken);
+          }
+      }
+  });
+}

2a86ff44f4536e9c6635799903d91faa1aa34fad
infinitepure <matt.j.cheah@gmail.com>
2017-11-26 00:40:34 +1100

    Update timers to take dropdown box values for timer duration

--- tracker/static/js/tracker-new.js
+++ tracker/static/js/tracker-new.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
-  var mins = 1;
-  var seconds = 0;
+  var mins = 25;
+  var pausedSecs = 0;
+  var tomatoTime = 25;
+  var breakTime = 5;
+  var tomatoSet = 4;
   var elapsed = 0;
   var interval;
-  var play = true;
-  var timers = [1];
+  var isPaused = true;
+  var isStart = true;
+  var tomatoNum = 1;
+  var isTomato = true;
   
+  $('#tomato-duration').on('change', function(e) {
+    tomatoTime = $(this).val();
+    if (isStart) {
+      mins = tomatoTime;
+      $('.timer p').text(`${formatDigit(mins)}:00`)
+    }
+  });
+
+  $('#break-duration').on('change', function(e) {
+    breakTime = $(this).val();
+  });
+
+  $('#tomato-set').on('change', function(e) {
+    tomatoSet = $(this).val();
+  });
+
   $('.timer').click(function() {
-    if (play) {
-      play = false;
+    isStart = false;
+    if (isPaused) {
+      isPaused = false;
       timer();
     }
     else {
       clearInterval(interval);
-      seconds = elapsed;
-      play = true;
-    }
-  });
-
-  $('.timer p').text(`${mins}:00`)
-
-  $('.schedule-add').on('keyup', function(e) {
-    if (e.keyCode == 13) {
-      var value = Number($('.schedule-add').val());
-      if (value > 0 && value <= 120 && Number.isInteger(value)) {
-        $('.schedule').append(
-          `<div class="schedule-time">${value}</div>`
-        )
-        timers.push(value);
-      }
+      pausedSecs = elapsed;
+      isPaused = true;
     }
   });
 
@@ -40,7 +48,7 @@ $(document).ready(function() {
     interval = setInterval(function() {
       var time = new Date().getTime() - start;
       
-      elapsed = (Math.floor(time / 1000) + seconds) % 60;
+      elapsed = (Math.floor(time / 1000) + pausedSecs) % 60;
       secs = 59 - elapsed;
       if (secs === 59) {
         if (change) {
@@ -54,23 +62,37 @@ $(document).ready(function() {
       
       if (mins < 0) {
         clearInterval(interval);
-        timers.shift();
-        $('.schedule-time:eq(0)').remove();
-        if (timers.length > 0) {
-          mins = timers[0];
-          secs = 0;
-          interval = timer();
-        }
+        mins = isTomato ? breakTime : tomatoTime;
+        secs = 0;
+        pausedSecs = 0;
+        isTomato = !isTomato;
+        timer();
       }
       else {
-        $('.timer p').text(mins + ':' + secs);
+        $('.timer p').text(`${formatDigit(mins)}:${formatDigit(secs)}`);
       }
     })
   }
 
-  
+  function addTimeOptions() {
+    for (var i=1; i<=120; i++) {
+      $('.setting').append(
+        `<option value="${i}">${i}</option>`
+      )
+    }
+    $('#tomato-duration option[value="25"]').prop('selected', true)
+    $('#break-duration option[value="5"]').prop('selected', true)
+    $('#tomato-set option[value="4"]').prop('selected', true)
+  }
 
+  addTimeOptions();
 
-  
-  
+  /**
+   * Returns the given number as a string, formatted to be two digits.
+   * @param {number} n - The number to be formatted
+   */
+  function formatDigit(n) {
+    return n > 9 ? '' + n : '0' + n;
+  }
+
 });

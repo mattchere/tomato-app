c493de4861b37e0c2ae6019a83a814830524abe3
infinitepure <matt.j.cheah@gmail.com>
2017-12-04 17:28:10 +1100

    Remove static root from urls

--- tomatoapp/urls.py
+++ tomatoapp/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.conf import settings
 from django.contrib import admin
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('tracker.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
